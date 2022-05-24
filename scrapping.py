cde435a6a67a8afb59c94889930a2024f87580aa
atronee <otavio.oam@protonmail.com>
2022-05-24 19:04:17 -0300

    Crie o Branch 'scrapping' para modificar a estrutura de funcionamento da pesquisa em html

--- scrapping.py
+++ scrapping.py
@@ -6,14 +6,26 @@ def encontra_ativos(url):
         content = html_file.read()
         #html_text = requests.get(url).text  Real projeto abrir um url
         soup = BeautifulSoup(content, 'lxml')
-        ativos = soup.find_all('tr', class_="linha")
-        dicionário = {}
-        for index, ativo in enumerate(ativos):   # Enumera cada tag "tr"
-            nome_compania = ativo.find('td', class_='compania').text.replace(' ','')
-            ações = ativo.find('td', class_='nomeacao').text.replace(' ','')
-            num_ações = ativo.find('td', class_='quantidadeacoes').text.replace(' ','')
-            dicionário[index] = {"nome da compania/ Pais":nome_compania, 
-                                 "nome do Ativo":ações,
-                                 "Quantidade":num_ações}
-    return dicionário
+        carteira = {} # Um Dicionário que contém todas as informações de ações e moedas
+        dicionario_acoes = {} # Dicionário com as informações de uma ação
+        dicionario_moedas = {} #Dicionário com as informações de uma moeda
+        lista_acoes = [] # Lista com os Dicionários de cada ação
+        lista_moedas = [] # Lista com os Dicionários de cada moeda
+        acoes = soup.find_all('div', class_="acao") # Encontra todas as tags div com classe "acao"
+        for index, ativo in enumerate(acoes):   # Enumera cada tag "tr"
+            nome_acao = ativo.find('td').text.replace(' ','')
+            num_acoes = ativo.find('td').text.replace(' ','')
+            dicionario_acoes[index] = {"Ação":nome_acao,
+                                       "Quantidade":num_acoes}
+            lista_acoes.append(dicionario_acoes) # Adiciona o dicionário de cada ação a lista de ações
+        moedas = soup.find_all('div', class_="moeda") # Encontra todas as tags div com classe "moeda"
+        for index, ativo in enumerate(moedas):   # Enumera cada tag "tr"
+            nome_acao = ativo.find('td').text.replace(' ','')
+            num_acoes = ativo.find('td').text.replace(' ','')
+            dicionario_moedas[index] = {"Moeda":nome_acao,
+                                        "Quantidade":num_acoes}
+            lista_moedas.append(dicionario_moedas) # Adiciona o dicionário de cada moeda a lista de moedas
+        carteira["acao"] = lista_acoes 
+        carteira["moeda"] = lista_moedas
+    return carteira
     # Cria um dicionário com chave para cada linha da tabela

2b4feb4359de8d3b9b9987b650c77da1889b2084
atronee <otavio.oam@protonmail.com>
2022-05-31 08:26:45 -0300

    Adicione comentários, adicione o título da carteira no dicionário

--- scrapping.py
+++ scrapping.py
@@ -1,9 +1,12 @@
-from bs4 import BeautifulSoup # Biblioteca para Webscraping
-import requests # Biblioteca para acessar sites reais
+from bs4 import BeautifulSoup
+import requests
 
-def estrutura(lista_acoes,lista_moedas, carteira): # Organiza os dados em um dicionário com uma lista de dicionarios para ações e para moedas
-    acoes_lista = []
-    for i in range(0,len(lista_acoes), 2):
+def estrutura(lista_acoes,lista_moedas, carteira, título):  
+    # Organiza os dados em {acao:[{nome:"nome",quantidade:"número"}],
+    # moeda:[{nome:"nome",quantidade:"número"}],
+    # título:"título"}
+    acoes_lista = []                                                     
+    for i in range(0,len(lista_acoes), 2):                               
         disc_acoes = {}
         disc_acoes["Nome"] = lista_acoes[i]
         disc_acoes["Quantidade"] = lista_acoes[i+1]
@@ -16,26 +19,34 @@ def estrutura(lista_acoes,lista_moedas, carteira): # Organiza os dados em um dic
         disc_moedas["Quantidade"] = lista_moedas[i+1]
         moedas_lista.append(disc_moedas)
     carteira["moeda"] = moedas_lista
+    carteira["título"] = título
     return carteira
 
-lista_acoes = [] # Lista com as linhas dentro da tabela de ações
-lista_moedas =[] # Lista com as linhas dentro da tabela de Moedas
-carteira ={}
+lista_acoes = [] # Lista com as células dentro da tabela de ações
+lista_moedas =[] # Lista com as células dentro da tabela de Moedas
+carteira ={}     # Dicionário que vai ser retornado para a interface
 
 def encontra_ativos(url):
-    content = requests.get(url).text
+    content = requests.get(url).text 
     soup = BeautifulSoup(content, 'lxml')
-    todas_linhas = soup.find_all('tr') # Encontra todas as tags tr, que definem novas linhas
+    h1 = soup.find('h1')
+    title = soup.find('title')      # Aqui o código vai buscar pelo título da Carteira
+    if h1 is not None:              # A preferencia ao h1
+        titulo = h1.text
+    elif title is not None and h1 is None:
+        titulo = title.text
+
+    todas_linhas = soup.find_all('tr')        # Encontra todas as tags tr, que definem novas linhas
     for linha in todas_linhas:
-        todas_celulas = linha.find_all('td') # Encontra todas as tags td, celulas da tabela sem contar cabeçalho
+        todas_celulas = linha.find_all('td')  # Encontra todas as tags td, celulas da tabela sem contar cabeçalho
         for celula in todas_celulas:
-            if celula is not None:
-                celulas_acoes = celula.find_parents("div", class_="acao") # Verifica se a tag div com classe acao é pai da tag td
+            if celula is not None:                                          # A lógica é se uma célula não é nula, o programa vai
+                celulas_acoes = celula.find_parents("div", class_="acao")   # Verificar se a tag div com classe acao é pai da tag td,
                 celulas_moedas = celula.find_parents("div", class_="moeda") # Verifica se a tag div com classe moeda é pai da tag td
-                for i in celulas_acoes:
+
+                for i in celulas_acoes:                                     # As iterações para adicionar cada célula em sua lista
                     lista_acoes.append(celula.text)
                 for j in celulas_moedas:
                     lista_moedas.append(celula.text)
 
-    estrutura(lista_acoes,lista_moedas, carteira)
-    
+    estrutura(lista_acoes,lista_moedas, carteira, titulo)

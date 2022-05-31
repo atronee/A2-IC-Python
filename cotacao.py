5c3c45689956a8424e4fa37d9d55f856fea2ee9e
FabricioVenturim <fabriciodalviventurim@gmail.com>
2022-05-31 14:59:14 -0300

    Adicione a base do módulo 'cotacao'

--- cotacao.py
+++ cotacao.py
@@ -1,18 +1,37 @@
-from datetime import datetime
 import yfinance as yf
-import pandas as pd
 
 #Site: https://finance.yahoo.com/
 
 #Carteira Exemplo
-carteira = {"acao":[{"Nome":"PETR4.SA", "Quantidade":"1000"},{"Nome":"VALE3.SA", "Quantidade":"1000"}], 
-            "moeda": [{"Nome":"USDBRL=X", "Quantidade":"100"}, {"Nome":"EURBRL=X", "Quantidade":"500"}]}
+carteira = {"acao":[{"Nome":"AAPL34.SA", "Quantidade":"15"},{"Nome":"MSFT", "Quantidade":"18"}, {"Nome":"NVDA", "Quantidade":"19"}, {"Nome":"INTC", "Quantidade":"12"}, {"Nome":"AMD", "Quantidade":"34"}], 
+           "moeda": [{"Nome":"EURBRL=X", "Quantidade":"100"}, {"Nome":"USDBRL=X", "Quantidade":"500"}]}
 
-for tipo, ativos in carteira.items():
-    for ativo in ativos:
-        ticker = yf.Ticker(ativo["Nome"])
-        cotacao_do_dia = ticker.history(period="1d")
-        ativo["preco_atualizado"] = cotacao_do_dia.iloc[0]["Close"] # Coisa do pandas
-        ativo["preço_histórico"] = ticker.history(period="1y")   # tipo <class 'pandas.core.frame.DataFrame'>
+def cotacao(carteira):
+    for ativos in carteira.values():
+        for ativo in ativos:
+            ticker = yf.Ticker(ativo["Nome"])
+            ticker_info = ticker.info
+            preco_do_dia = ticker_info["regularMarketPrice"] # float
+            cotacao_do_ano = ticker.history(period="1y", interval="1d") #Dataframe com o histórico do ativo
+            preco_historico = cotacao_do_ano.loc[:,['Close']] #Dataframe com o histórico do fechamento do ativo 
+            
+            moeda_do_ativo = ticker_info["currency"] # String
 
-print(carteira)
+            if moeda_do_ativo != "BRL":
+                cotacao_br = converte_moeda_de_cotacao(moeda_do_ativo, preco_historico)
+                preco_do_dia = preco_do_dia * cotacao_br["preco_atualizado"]
+                preco_historico = preco_historico * cotacao_br["preco_historico"]
+
+            ativo["preco_atualizado"] = preco_do_dia 
+            ativo["preco_historico"] = preco_historico 
+    return carteira
+
+def converte_moeda_de_cotacao(moeda_estrangeira, cotacao_ativo):
+    moeda_br = moeda_estrangeira + "BRL=X"
+    convecao_moeda = {}
+    convecao_moeda["preco_atualizado"] = yf.Ticker(moeda_br).info["regularMarketPrice"]
+    convecao_moeda["preco_historico"] = yf.Ticker(moeda_br).history(start=cotacao_ativo.index[0], stop=cotacao_ativo.index[-1], interval="1d").loc[:,['Close']]
+    return convecao_moeda
+
+cota = cotacao(carteira)
+print(cota)
